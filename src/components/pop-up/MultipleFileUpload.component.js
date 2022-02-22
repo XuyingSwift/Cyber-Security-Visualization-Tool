29a0e45186583353d2b6d77e6b0f68bb4c8354f3
XuyingSwift <xuyingswift@gmail.com>
2022-02-21 18:41:14 -0700

    can upload by quarter numbers now

--- src/components/pop-up/MultipleFileUpload.component.js
+++ src/components/pop-up/MultipleFileUpload.component.js
@@ -1,26 +1,93 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import {  setProjects } from '../../redux/piqueTree/PiqueTree.actions';
+import {  setProjects, setQuarters } from '../../redux/piqueTree/PiqueTree.actions';
 import { readAllFiles } from '../../utils/fileUpload.utils';
 
-import { LoaderWrapper, Label, Input, ProgressResult} from './MultipleFileUpload.styles';
-import { selectProjects } from '../../redux/piqueTree/PiqueTree.selector';
+import { LoaderWrapper, Label, Input, SubmitButton, ResetButton} from './MultipleFileUpload.styles';
+import { selectProjects, selectQuarters } from '../../redux/piqueTree/PiqueTree.selector';
 import { Line } from 'rc-progress';
 import { Green } from '../../utils/color';
 
+import FormInput from '../formInput/FormInput.component';
+import { useAlert } from 'react-alert'
 
-const MultipleFilesUpload = ({projects, setProjects}) => {
 
+
+const MultipleFilesUpload = ({projects, setProjects, quarters, setQuarters}) => {
+    const alert = useAlert();
     const [progress, setProcess] = React.useState(0)
+    const [quarterNumber, setQuarterNumber] = React.useState('');
+    const [quarterFiles, setQuarterFiles] = React.useState([]);
+    const [submitting, setSubmitting] = React.useState(false);
+    const [file, setFile] = React.useState(null);
+
 
     const handleUpload = async (e) => {
         let allFiles = [];
-        [...e.target.files].filter(file => file.size !== 0).map(file=> allFiles.push(file))
+        [...e.target.files].filter(file => file.size !== 0).map(file=> allFiles.push(file));
         const results = await readAllFiles(allFiles, setProcess);
-        setProjects(results)
+        setQuarterFiles(results)
+        setFile(results.pop());
     }
+    console.log("filesss", quarterFiles);
+       // onChange to handle version input
+    const handleQuarterNumber = e => {
+        setQuarterNumber(e.target.value)
+        if (file !== null) {
+            file["QuarterNumber"]=e.target.value
+        } else{
+            alert.show("file content is null")
+        }
+    }
+
+    console.log("quarter number", quarterNumber)
+
+    // onClick to handle submitting
+    const handleSubmitting = () => {setSubmitting(!submitting)}
+
+    const checker = (arr, target) => target.every(v => arr.includes(v));
+
+    const checkFileInProjects =(arr, target) => {
+        const checker = target.every(f => arr.includes(f));
+        return checker;
+    }
+
+    console.log("repeated file", checkFileInProjects(projects, quarterFiles));
+
+    //validates inputs
+    const handleValidation = () => {
+        if (quarterFiles === null ) {
+            alert.show("You must select a file to upload!");
+        } 
+        if (quarterNumber === '') {
+            alert.show(" you mush enter a quarter number")
+        }
+        if (checkFileInProjects(projects, quarterFiles)) {
+            alert.show("These files already exist, please select a different one")
+        }
+        if (quarters.includes(quarterNumber)) {
+            alert.show("this quarter number already exists, please give it a different quarter number")
+        }
+    }
+
+
+     // onClick to setprojects setVersions
+     const handleSubmit = () => {
+        handleValidation()
+        if (quarterNumber !== '' && quarterFiles !== null && !checker(projects, quarterFiles) && !quarters.includes(quarterNumber)) {
+            quarters.push(quarterNumber);
+            const combined = [...projects, ...quarterFiles]
+            setQuarters(quarters)
+            setProjects(combined)
+            handleSubmitting()
+        }
+    }
+
    
+    console.log("quarters ", quarters)
+    console.log("submmitting", submitting)
+
     return (
         <div>
             <LoaderWrapper>
@@ -33,6 +100,23 @@ const MultipleFilesUpload = ({projects, setProjects}) => {
                         onChange={handleUpload}
                     />
                 </Label>
+                
+                <FormInput
+                    type='text'
+                    label={"Quarter Number: Q1"}
+                    name="quarter"
+                    value={quarterNumber}
+                    onChange={handleQuarterNumber}
+                />
+                <SubmitButton
+                        type='submit' 
+                        onClick={() => {handleSubmit()}}
+                        disabled={submitting}
+                        submit={submitting}
+                    >
+                        Submit
+                </SubmitButton>
+                {submitting ? <ResetButton onClick={handleSubmitting}>Reset</ResetButton> : null}
             </LoaderWrapper>
                 {projects && progress ? (
                     projects.map((file, index) => {
@@ -51,11 +135,13 @@ const MultipleFilesUpload = ({projects, setProjects}) => {
 }
 
 const mapStateToProps = createStructuredSelector({
-  projects: selectProjects
+  projects: selectProjects,
+  quarters: selectQuarters
 })
 
 const mapDispatchToProps = dispatch => ({
     setProjects: data => dispatch(setProjects(data)),
+    setQuarters: data => dispatch(setQuarters(data))
 })
 
 
